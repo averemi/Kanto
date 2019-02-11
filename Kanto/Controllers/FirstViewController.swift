873d6513065aae9bbda64dafb3fff3e941afb31b
Anastasia Veremiichyk <anastasia.veremi@gmail.com>
2019-02-12 00:56:07 +0200

    initial commit

---
+++ Kanto/Controllers/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Kanto
+//
+//  Created by Anastasia Veremiichyk on 2/12/19.
+//  Copyright © 2019 Anastasia Veremiichyk. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Kanto/Controllers/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Kanto
+//
+//  Created by Anastasia Veremiichyk on 2/12/19.
+//  Copyright © 2019 Anastasia Veremiichyk. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
